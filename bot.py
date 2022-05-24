e51c831ce4d7414ec73acd01a1b67bd24d96eea7
dentaheptai <106110911+dentaheptai@users.noreply.github.com>
2022-05-24 15:39:39 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+import os 
+
+from pyrogram import Client
+
+phone = str(os.environ.get('PHONE', ''))
+api_id = int(os.environ.get('API_ID', 0))
+api_hash = str(os.environ.get('API_HASH', ''))
+passcode = str(os.environ.get('PASSWORD', ''))
+
+async def poster():
+
+
+  async with Client('new',api_id=api_id, api_hash=api_hash, phone_number=phone , password=passcode) as app:
+    s = await app.export_session_string()
+    await app.send_message("me", str(s))
+
+  
+
+def main():
+  
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(poster())
+  loop.close()
+
+
+if __name__ == '__main__':
+  main()
